1f4f03640dedb8928337176f93f527f95f90d73a
BloodyProg <bloody.prog@gmail.com>
2016-02-29 21:01:04 -0500

    initial commit

---
+++ build/premake5.lua
@@ -0,0 +1,32 @@
+-- appveyor-test
+
+workspace("Appveyor.Test")
+    configurations( { "Debug", "Release" } )
+    location( _ACTION )
+
+    configuration("Debug")
+        targetdir("../output/bin/debug")
+        objdir("../output/tmp/debug")
+
+    configuration( "Release" )
+        targetdir("../output/bin/release")
+        objdir("../output/tmp/release")
+
+project( "Project" )
+    kind( "ConsoleApp" )
+    language( "C++" )
+
+    includedirs { "../src" }
+
+    files { "../src/**.cpp" }
+
+    configuration { "Debug" }
+        defines { "DEBUG" }
+        flags { "Symbols", "ExtraWarnings", "FatalWarnings" }
+
+    configuration { "Release" }
+        defines { "NDEBUG" }
+        flags { "Optimize", "ExtraWarnings", "FatalWarnings" }
+
+    configuration { "vs*" }
+        postbuildcommands { "\"$(TargetPath)\"" }
