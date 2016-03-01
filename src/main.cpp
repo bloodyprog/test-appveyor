c50e0d1c38b506f270500b2d9574b99e1616f2ed
BloodyProg <bloody.prog@gmail.com>
2016-02-29 21:09:45 -0500

    build fix, gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-int main(int argc, char** argv)
+int main(int /*argc*/, char** /*argv*/)
 {
-    std::cout << "Hello AppVoyor!" << std::endl;
+    std::cout << "Hello AppVeyor!" << std::endl;
 
     return 0;
 }
