516e6752254c570327f24373d61d23d8ac77d779
BloodyProg <bloody.prog@gmail.com>
2016-03-28 19:01:34 -0400

    test results

--- src/TestCpp/TestFile.cpp
+++ src/TestCpp/TestFile.cpp
@@ -1,6 +1,16 @@
 #include <catch.hpp>
 
-TEST_CASE("Test")
+TEST_CASE("Test_Case_A")
 {
     CHECK(true);
+}
+
+TEST_CASE("Test_Case_B")
+{
+    CHECK(true);
+}
+
+TEST_CASE("Test_Case_C")
+{
+    CHECK(false);
 }
