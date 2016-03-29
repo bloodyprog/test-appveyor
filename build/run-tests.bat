ad607b02789e42e18f9fbca4ff26d2b95771469d
BloodyProg <bloody.prog@gmail.com>
2016-03-28 20:13:21 -0400

    test

--- build/run-tests.bat
+++ build/run-tests.bat
@@ -1,8 +1,13 @@
 @echo off
 
-pushd %~dp0/../output/bin/debug
+pushd %~dp0\..\output\bin\debug
 
 TestCpp.exe --reporter junit --out TestCpp.results.xml
 TestCSharp.exe
 
 popd
+
+if %ERRORLEVEL% neq 0 (
+    echo TestErrors
+    exit 0
+)
